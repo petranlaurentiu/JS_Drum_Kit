9df7d6f6b5faaf6364e3c400c8f4928beaf820b6
Petran Laurentiu <56577088+petranlaurentiu@users.noreply.github.com>
2021-01-11 14:33:38 +0200

    Added Beautify ext for code formatter

---
+++ index.js
@@ -0,0 +1,56 @@
+// Detecting Button Press
+let numberOfButtons = document.querySelectorAll(".drum").length; //vom selecta toate clasele cu .drum si verifica cate sunt cu length
+for (let i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+    });
+}
+// Detecting Keyboard Press
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "k":
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "l":
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        default:
+    }
+}
